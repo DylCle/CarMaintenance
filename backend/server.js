a7dbb5b120e3bb8e393601852f831faa438599f1
unknown <newrussianguy12@gmail.com>
2023-07-26 20:19:53 -0300

    implemented letters in url

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,28 @@ const {router: resetPwdEmail, email} = require('./routes/resetpassword-route.js'
 const newPwdPg = require('./routes/newPasswordPage-route.js')
 const signUpRoute = require('./routes/signup-routes.js');
 const checkEmail = require('./routes/checkemail-routes.js')
-let ranId = Math.floor(Math.random() * 10000) + 3;
-module.exports = ranId;
-console.log(ranId);
-require('dotenv').config();
 
+require('dotenv').config();
+function ranId(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+let id = ranId(15);
+module.exports = id;
+
+console.log(id);
 app.use(bodyParser.json());
 app.use(express.json());
 
 app.use('/send-reset-pwd-email', resetPwdEmail);
-app.use(`/${ranId}/:username`, newPwdPg)
+app.use(`/${id}/:username`, newPwdPg)
 app.use('/login', loginRoute);
 app.use('/check-field', checkEmail);
 app.use('/signup', signUpRoute);
